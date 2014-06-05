766126d07a6d644b8ac3923894857b53f5024d27
Kevin P. Dyer <kpdyer@gmail.com>
2014-06-05 16:21:30 -0700

    revert addition of -fPIC to linker flags

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ fte_cDFA = Extension('fte.cDFA',
                                          ],
                      extra_link_args=['thirdparty/'+re2_dir+'/obj/libre2.a',
                                       '-Wl,-undefined,dynamic_lookup',
-                                      '-fPIC',
                                       ],
                      libraries=libraries,
                      sources=['fte/rank_unrank.cc', 'fte/cDFA.cc'])
